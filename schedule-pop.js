bfde0baf4d13d772d908279848de355bb9fb19ab
Cameron <cameron@harves.net>
2024-10-17 00:37:05 +0800

    Wrap scheduling into 'getRealBe' function

--- schedule-pop.js
+++ schedule-pop.js
@@ -1,13 +1,20 @@
 import { getRandomTime } from './random-time.js';
-const schedule = require('node-schedule');
-const { popTime } = require('./config.json');
+import config from './config.json' assert { type: 'json' };
+import schedule from 'node-schedule';
+import { sendRealBeNotification } from './discord-bot.js';
 
-// generate today's pop at 3am daily 
-const daily_generate_pop = schedule.scheduleJob( { hour: 3 }, function(){
-    if (!current_pop) { 
-        current_pop = schedule.scheduleJob( getRandomTime(popTime.min,popTime.max), function(){
-            console.log("Yooo it's time for RealBe !!!")
-        });
-    };
-    current_pop.reschedule( getRandomTime(popTime.min,popTime.max) )
-});
+// Generate today's pop at 3am daily 
+function getRealBe(){
+    const generate_daily_pop = schedule.scheduleJob( { hour: config.schedulerTime }, function(){
+        let POP_TIME = getRandomTime(config.popTime.min, config.popTime.max);
+        console.log("Generating today's RealBe pop time!");
+        console.log(POP_TIME);
+        const current_pop = schedule.scheduleJob(POP_TIME, function(){
+            console.log("Yooo it's time for RealBe !!!");
+            sendRealBeNotification();
+            current_pop.cancel()
+        })
+    })
+};
+    
+export { getRealBe }
