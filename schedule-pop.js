eae8ad5f91665711a36e622b045bef0886654486
Cameron <cameron@harves.net>
2024-10-18 00:12:42 +0800

    Remove testing console.logs

--- schedule-pop.js
+++ schedule-pop.js
@@ -3,14 +3,10 @@ import config from './config.json' with { type: 'json' };
 import schedule from 'node-schedule';
 import { sendRealBeNotification } from './discord-bot.js';
 
-// Generate today's pop at 3am daily 
 function getRealBe(){
     const generate_daily_pop = schedule.scheduleJob( { hour: config.schedulerTime }, function(){
         let POP_TIME = getRandomTime(config.popTime.min, config.popTime.max);
-        console.log("Generating today's RealBe pop time!");
-        console.log(POP_TIME);
         const current_pop = schedule.scheduleJob(POP_TIME, function(){
-            console.log("Yooo it's time for RealBe !!!");
             sendRealBeNotification();
             current_pop.cancel()
         })
