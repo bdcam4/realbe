0ba24c329ff43c23677b71c5831be8e14b3e3e6d
Cameron <cameron@harves.net>
2024-10-20 16:33:41 +0800

    Fix daily scheduler cron to trigger only once

--- schedule-pop.js
+++ schedule-pop.js
@@ -4,13 +4,12 @@ import schedule from 'node-schedule';
 import { sendRealBeNotification } from './discord-bot.js';
 
 function getRealBe(){
-    const generate_daily_pop = schedule.scheduleJob( { hour: config.schedulerTime }, function(){
+    const generate_daily_pop = schedule.scheduleJob( { hour: config.schedulerTime, minute: 0 }, function(){
         let RANDOM_TIME = getRandomTime(config.popTime.min, config.popTime.max);        
         let POP_TIME = new Date();
         POP_TIME.setHours(RANDOM_TIME.hour);
         POP_TIME.setMinutes(RANDOM_TIME.minute);
         POP_TIME.setSeconds(RANDOM_TIME.second);
-        console.log(`RealBe scheduled for ${POP_TIME.toString()}`);
 
         const current_pop = schedule.scheduleJob(POP_TIME, function(){
             sendRealBeNotification()
